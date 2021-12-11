f5354064fcb82a8a3815dbb0acef0c851d1fd063
Tushar Modak <tushar.modak@bjitgroup.com>
2021-12-12 02:22:35 +0600

    Second commit

--- Function.js
+++ Function.js
@@ -1,8 +1,9 @@
+//Local and global scope VAR and LET Keyword
 var carName = "BMW";
 
 function cars()
 {
-    let carName = "Mercedes";
+    let carName = "Mercedes"; 
     console.log(carName);
 }
 
